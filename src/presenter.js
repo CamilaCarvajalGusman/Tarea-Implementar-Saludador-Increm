4c81b4d357eeae94b95602af7c3589fdae27ae5f
Camila <camila.carvajal@ucb.edu.bo>
2025-08-21 09:02:25 -0400

    implementado Saludar por genero

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,10 @@
 import saludar from "./saludador";
 const nombre= document.querySelector("#nombre");
+const genre=document.querySelector("#genre");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  div.innerHTML = "<h2>" + saludar(nombre.value)+ "</h2>";
+  div.innerHTML = "<h2>" + saludar(nombre.value, genre.value)+ "</h2>";
 });
--- src/saludador.js
+++ src/saludador.js
@@ -1,12 +1,18 @@
-function saludar(nombre){
+function saludar(nombre, genre){
     const hoy = new Date();
+    let gen="";
+    if(genre==='Femenino'){
+        gen="señorita"
+    }else{
+        gen="señor"
+    }
     if(hoy.getHours()>12&&hoy.getHours()<19){
-        return "Buenas tardes "+nombre;
+        return "Buenas tardes "+gen+" "+nombre;
     }else{
         if(hoy.getHours()<12){
-            return "Buenos días "+nombre;
+            return "Buenos días "+gen+" "+nombre;
         }else{
-            return "Buenas noches "+nombre;
+            return "Buenas noches "+gen+" "+nombre;
         }
     }
     //  "hoy es ",hoy.toDateString(), " - ",hoy.getHours(),":",hoy.getMinutes()
