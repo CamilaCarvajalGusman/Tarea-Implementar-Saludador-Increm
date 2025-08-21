1acd32ca30f6b61ad8c42247c4e857c198d33252
Camila <camila.carvajal@ucb.edu.bo>
2025-08-21 08:45:37 -0400

    implementado Saludar de acuerdo a la hora
    Buenos dias, buenas tardes, etc

--- src/saludador.js
+++ src/saludador.js
@@ -1,6 +1,14 @@
 function saludar(nombre){
     const hoy = new Date();
-    return "Hola "+ nombre;
+    if(hoy.getHours()>12&&hoy.getHours()<19){
+        return "Buenas tardes "+nombre;
+    }else{
+        if(hoy.getHours()<12){
+            return "Buenos días "+nombre;
+        }else{
+            return "Buenas noches "+nombre;
+        }
+    }
     //  "hoy es ",hoy.toDateString(), " - ",hoy.getHours(),":",hoy.getMinutes()
 }
 export default saludar;
