7bfd86744a76b57885559cc293ee2543e27d992b
Camila <camila.carvajal@ucb.edu.bo>
2025-08-24 11:23:02 -0400

    feat - saludar en diferentes idiomas

--- src/presenter.js
+++ src/presenter.js
@@ -2,10 +2,11 @@ import saludar from "./saludador";
 const nombre= document.querySelector("#nombre");
 const genre=document.querySelector("#genre");
 const age=document.querySelector("#age");
+const language = document.querySelector("#language");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  div.innerHTML = "<h2>" + saludar(nombre.value, genre.value, age.value)+ "</h2>";
+  div.innerHTML = "<h2>" + saludar(nombre.value, genre.value, age.value, language.value)+ "</h2>";
 });
--- src/saludador.js
+++ src/saludador.js
@@ -1,28 +1,84 @@
-function saludar(nombre, genre, age){
-    const hoy = new Date();
-    let gen="";
-    if(genre==='Femenino'&& age<30){
-        gen="señorita"
-    }else{
-        if(genre==='Femenino'&&age>30){
-            gen="señora"
-        }else{
-            if(genre="Masculino"&&age>30){
-                gen="señor"
-            }else{
-                gen="joven"
-            }
-        }
+function saludar(nombre, genre, age, language) {
+  console.log("Camila Carvajal");
+  const hoy = new Date();
+  let gen = "";
+
+  if (language === 's') {
+    if (genre === 'Femenino' && age < 30) {
+      gen = "señorita";
+    } else if (genre === 'Femenino' && age >= 30) {
+      gen = "señora";
+    } else if (genre === "Masculino" && age >= 30) {
+      gen = "señor";
+    } else {
+      gen = "joven";
     }
-    if(hoy.getHours()>12&&hoy.getHours()<19){
-        return "Buenas tardes "+gen+" "+nombre;
-    }else{
-        if(hoy.getHours()<12){
-            return "Buenos días "+gen+" "+nombre;
-        }else{
-            return "Buenas noches "+gen+" "+nombre;
-        }
+
+    if (hoy.getHours() < 12) {
+      return "Buenos días " + gen + " " + nombre;
+    } else if (hoy.getHours() < 19) {
+      return "Buenas tardes " + gen + " " + nombre;
+    } else {
+      return "Buenas noches " + gen + " " + nombre;
     }
-    //  "hoy es ",hoy.toDateString(), " - ",hoy.getHours(),":",hoy.getMinutes()
+
+  } else if (language === 'e') {
+    if (genre === 'Femenino' && age < 30) {
+      gen = "young lady";
+    } else if (genre === 'Femenino' && age >= 30) {
+      gen = "lady";
+    } else if (genre === "Masculino" && age >= 30) {
+      gen = "mister";
+    } else {
+      gen = "young one";
+    }
+
+    if (hoy.getHours() < 12) {
+      return "Good morning " + gen + " " + nombre;
+    } else if (hoy.getHours() < 19) {
+      return "Good afternoon " + gen + " " + nombre;
+    } else {
+      return "Good evening " + gen + " " + nombre;
+    }
+
+  } else if (language === 'p') {
+    if (genre === 'Femenino' && age < 30) {
+      gen = "senhorita";
+    } else if (genre === 'Femenino' && age >= 30) {
+      gen = "senhora";
+    } else if (genre === "Masculino" && age >= 30) {
+      gen = "senhor";
+    } else {
+      gen = "jovem";
+    }
+
+    if (hoy.getHours() < 12) {
+      return "Bom dia " + gen + " " + nombre;
+    } else if (hoy.getHours() < 19) {
+      return "Boa tarde " + gen + " " + nombre;
+    } else {
+      return "Boa noite " + gen + " " + nombre;
+    }
+
+  } else if (language === 'f') {
+    if (genre === 'Femenino' && age < 30) {
+      gen = "mademoiselle";
+    } else if (genre === 'Femenino' && age >= 30) {
+      gen = "madame";
+    } else if (genre === "Masculino" && age >= 30) {
+      gen = "monsieur";
+    } else {
+      gen = "jeune";
+    }
+
+    if (hoy.getHours() < 12) {
+      return "Bonjour " + gen + " " + nombre;
+    } else if (hoy.getHours() < 19) {
+      return "Bon après-midi " + gen + " " + nombre;
+    } else {
+      return "Bonsoir " + gen + " " + nombre;
+    }
+  }
+
 }
 export default saludar;
