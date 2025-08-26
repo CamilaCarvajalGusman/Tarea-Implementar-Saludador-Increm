b63d345bc85e4d11a8de7c313c63236471042737
Camila <camila.carvajal@ucb.edu.bo>
2025-08-26 09:33:06 -0400

    feat/multiplicar 2 números + test.js

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -1,8 +1,14 @@
-import sumar from "./sumador.js";
+import { sumar, multiplicar } from "./calculadora";
 
-describe("Sumar", () => {
+describe("Calculadora", () => {
   it("deberia sumar dos numeros", () => {
     expect(sumar(3, 2)).toEqual(5);
+  });
+  it("deberia multiplicar dos numeros", () => {
+    expect(multiplicar(3, 2)).toEqual(6);
+  });
+  it("deberia multiplicar dos numeros", () => {
+    expect(multiplicar(13, 2)).toEqual(26);
   });
 });
 
