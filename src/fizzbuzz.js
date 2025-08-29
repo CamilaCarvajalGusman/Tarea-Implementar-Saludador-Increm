dcd6e5eea4c14c46742d2c1f38a86becc0af0f6c
Camila <camila.carvajal@ucb.edu.bo>
2025-08-29 08:38:30 -0400

    feat: retornar fizz cuando es multiplo de 3

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,8 @@
-function convertirFizzBuzz(){
-    return "1";
+function convertirFizzBuzz(a) {
+    if (a % 3 != 0 && a % 5 != 0) {
+        return a.toString();
+    } else if (a % 3 == 0) {
+        return "fizz";
+    }
 }
 export default convertirFizzBuzz;
--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -4,4 +4,7 @@ describe("FizzBuzz", () => {
     it("debería convertir un número que no es múltiplo de 3 ni 5", () =>{
         expect(convertirFizzBuzz(1)).toEqual("1");
     });
+    it("debería retornar fizz cuando es multiplo de 3", () => {
+        expect(convertirFizzBuzz(9)).toEqual("fizz");
+    });
 });
